34eff6a422aa9d0db52618bef79c19a45bef0aa4
inuzukam <48746108+inuzukam@users.noreply.github.com>
2023-06-02 09:50:06 +0900

    apply black

--- test.py
+++ test.py
@@ -2,18 +2,19 @@ from auth0_component import login_button
 import streamlit as st
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-clientId = os.environ['clientId']
-domain = os.environ['domain']
+clientId = os.environ["clientId"]
+domain = os.environ["domain"]
 
-st.title('Welcome to Auth0-Streamlit')
+st.title("Welcome to Auth0-Streamlit")
 
 with st.echo():
-    user_info = login_button(clientId = clientId, domain = domain)
+    user_info = login_button(clientId=clientId, domain=domain)
     if user_info:
         st.write(f'Hi {user_info["nickname"]}')
         # st.write(user_info) # some private information here
-        
+
 if not user_info:
     st.write("Please login to continue")
